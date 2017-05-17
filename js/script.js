d957e3e7018a372b5c3ff478f001cdb2469d1b95
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-17 12:38:03 -0400

    ejercicio guiado termiado, twiter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
 function agregar(){
-	
+    var tareas = document.getElementById("tarea").value;
+    document.getElementById("tarea").value= "";
+    var cont = document.getElementById("contenedor");
+    var nuevasTareas = document.createElement("div");
+    var textoNuevaTarea = document.createTextNode(tareas);
+    var elementoContenedor = document.createElement("span");
+
+    if(tareas == null || tareas.length == 0){
+        alert("¡Error!, debe ingresar una tarea");
+        return false;
+    }
+
+    elementoContenedor.appendChild(textoNuevaTarea);
+    nuevasTareas.appendChild(elementoContenedor);
+    cont.appendChild(nuevasTareas);
+
+    var chck = document.createElement("input");
+    chck.type = "checkbox";
+    chck.setAttribute("class", "check");
+    var basura = document.createElement("span");
+    basura.classList.add("fa", "fa-trash-o");
+    var cora = document.createElement("span");
+    cora.classList.add("fa", "fa-heart");
+    nuevasTareas.appendChild(chck);
+    nuevasTareas.appendChild(basura);
+    nuevasTareas.appendChild(cora);
+
+    chck.addEventListener("click", function(){
+        elementoContenedor.classList.toggle("tachado");
+    })
+
+    basura.addEventListener("click", function(){
+        cont.removeChild(nuevasTareas);
+    })
+
+    cora.addEventListener("click", function(){
+        cora.classList.toggle("red");
+    })
+
+    
 }
+
 
 
 
